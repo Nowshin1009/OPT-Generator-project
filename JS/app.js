d04ddc900f7f2d9a44ff30850d0a77f9c151fbd6
nowshin1009 <nowshin35-1009@diu.edu.bd>
2024-06-02 21:48:39 +0600

    Dark mode

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,19 @@
-console.log(Nowshin)
+console.log("Connected");
+const switchBtnToggler = document.getElementById("switch-btn-toggler")
+
+
+
+//event handler :
+
+switchBtnToggler.addEventListener("click", function(){
+    var root = document.querySelector(":root");
+    var rootStyle = getComputedStyle(root);
+    const isWhite = rootStyle.getPropertyValue('--theme-bg');
+    console.log("isWhite");
+    if(isWhite == "#E2E0E1" ){
+        root.style.setProperty('--theme-bg','#3e3939');
+    } else {
+    root.style.setProperty('--theme-bg', '#E2E0E1');
+    }
+}) ;
+
