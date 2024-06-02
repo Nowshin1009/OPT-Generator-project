f0fc3e5f124ffed9b7b40c0daa2898bb6eb507cb
nowshin1009 <nowshin35-1009@diu.edu.bd>
2024-06-03 00:17:05 +0600

    Input fiel

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,33 @@
-console.log("Connected");
 const switchBtnToggler = document.getElementById("switch-btn-toggler")
+const generateOtpCode = document.getElementById("generate-otp-code")
+const otpInputList = document.getElementById("otp-input-list")
 
+
+
+function displayOtp(){
+    generateOtpCode.innerHTML = `
+    <div>Your OTP code 3209</div>
+    <button class="btn">reset</button>
+    <div>expires in 0s</div>  `
+}
+
+const checkOtp = (e) => {
+  const currentElement = e.target;
+  const currentValue = e.target.value;
+  const nextChild = currentElement.nextElementSibling;
+
+  // Forward to next childm:]
+  if (nextChild){
+    nextChild.focus();
+
+  }
+
+    
+    console.log(nextChild)
+}
 
 
 //event handler :
-
 switchBtnToggler.addEventListener("click", function(){
     var root = document.querySelector(":root");
     var rootStyle = getComputedStyle(root);
@@ -17,3 +40,6 @@ switchBtnToggler.addEventListener("click", function(){
     }
 }) ;
 
+otpInputList.addEventListener("input", checkOtp);
+
+setTimeout( displayOtp,1000)
